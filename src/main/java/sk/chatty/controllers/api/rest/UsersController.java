6be4570c6747dda83cf9f7f477fd6cfcd7e9f71f
Illia <ilyayrch@gmail.com>
2022-07-09 18:41:25 +0200

    database remake

--- src/main/java/sk/chatty/controllers/api/rest/UsersController.java
+++ src/main/java/sk/chatty/controllers/api/rest/UsersController.java
@@ -1,11 +1,15 @@
 package sk.chatty.controllers.api.rest;
 
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import sk.chatty.dao.UserDAO;
 import sk.chatty.models.User;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -13,6 +17,7 @@ import java.util.ArrayList;
  * Actions with user/users
  */
 
+//@Validated
 @RequestMapping("/api/v1")
 @RestController
 public class UsersController {
@@ -30,10 +35,10 @@ public class UsersController {
      * @return new User
      */
     @GetMapping("/createUser")
-    public User createNewUser(@RequestParam("username") String username) {
+    public User createNewUser(@RequestParam("username") String username) throws SQLException {
         User user = userDAO.createUser(username);
         if (user == null)
-            return new User(-1, null, null);
+            return new User();
         return user;
     }
 
@@ -44,10 +49,10 @@ public class UsersController {
      * @return Me
      */
     @GetMapping("/me")
-    public User getMe(@RequestParam("api_key") String api_key) {
-        User user = userDAO.getUser(api_key);
+    public User getMe(@RequestParam("api_key") String api_key) throws SQLException {
+        User user = userDAO.getMe(api_key);
         if (user == null)
-            return new User(-1, null, null);
+            return new User();
         return user;
     }
 
@@ -60,13 +65,14 @@ public class UsersController {
      */
     @GetMapping("/me/change")
     public User changeMe(@RequestParam("api_key") String api_key,
-                         @RequestParam("username") String username) {
-        User user = userDAO.getUser(api_key);
+                         @RequestParam("username") String username) throws SQLException {
+        User user = userDAO.getMe(api_key);
 
         if (user == null)
-            return new User(-1, null, null);
+            return new User();
 
         user.setUsername(username);
+        userDAO.updateUser(user);
         return user;
     }
 
@@ -77,10 +83,26 @@ public class UsersController {
      * @return new User
      */
     @GetMapping("/getUsers")
-    public ArrayList<User> getUsers(@RequestParam("api_key") String api_key) {
-        if (userDAO.getUser(api_key) == null)
+    public ArrayList<User> getUsers(@RequestParam("api_key") String api_key) throws SQLException {
+        if (userDAO.getMe(api_key) == null)
             return new ArrayList<>();
         return userDAO.getUsers(api_key);
+    }
+
+
+    /**
+     * Get user info
+     *
+     * @param api_key api_key
+     * @param uid user id
+     * @return new User
+     */
+    @GetMapping("/getUser")
+    public User getUser(@RequestParam("api_key") String api_key,
+                                   @RequestParam("uid") int uid) throws SQLException {
+        if (userDAO.getMe(api_key) == null)
+            return new User();
+        return userDAO.getUser(api_key, uid);
     }
 
 }
